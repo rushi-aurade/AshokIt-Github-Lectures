b2084c55516c8e866767234be9b984ad40ce040e
Rushikesh Shivraj Aurade <auraderushi@gmail.com>
2026-06-26 08:01:10 +0530

    peoples file edited successfully

--- Peoples.java
+++ Peoples.java
@@ -3,6 +3,6 @@ public static void main(String args[]){
 
   int age = 24;
   String name = "Rushikesh Shivraj Aurade";
-  System.out.println("My Name is "+ name + "and Age is " + age);
+  System.out.println("My Name is "+ name + "   and Age is " + age);
 }
 }
